0934e94c1561c9915ef392ea86144ecb7e51f172
Ryan Baker <ryan.shane.baker@gmail.com>
2020-06-09 19:27:02 -0400

    Added variables and notes

---
+++ character.js
@@ -0,0 +1,24 @@
+//Character name, string value
+var charName()=' ';
+
+//Character race, will have modifying values added to the below scores
+var charRace()
+
+//Character class, this is for gameplay mechanics
+var charClass()
+
+//these score will drive the modifier that will play into the game mechanics
+var dexScore ()
+var strScore ()
+var conScore ()
+var wisScore ()
+var intScore ()
+var chaScore ()
+
+//equals corresponding Score value minus 10 then divided by 2 rounded down
+var dexMod () 
+var strMod ()
+var conMod ()
+var wisMod ()
+var intMod ()
+var chaMod ()
