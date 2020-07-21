689970883eab0a3e9deb1ff875a77dfccf5c49da
Ryan Baker <ryan.shane.baker@gmail.com>
2020-07-21 17:33:03 -0400

    blah

--- character.js
+++ character.js
@@ -1,24 +1,38 @@
 //Character name, string value
-var charName()=' ';
+var charName='';
 
 //Character race, will have modifying values added to the below scores
-var charRace()
+var charRace ='';
 
 //Character class, this is for gameplay mechanics
-var charClass()
+var charClass='';
 
 //these score will drive the modifier that will play into the game mechanics
-var dexScore ()
-var strScore ()
-var conScore ()
-var wisScore ()
-var intScore ()
-var chaScore ()
+var dexScore 
+var strScore 
+var conScore 
+var wisScore 
+var intScore 
+var chaScore 
 
 //equals corresponding Score value minus 10 then divided by 2 rounded down
-var dexMod () 
-var strMod ()
-var conMod ()
-var wisMod ()
-var intMod ()
-var chaMod ()
+var dexMod = (dexScore-10)/2;
+
+
+var strMod 
+
+
+var conMod 
+
+
+var wisMod 
+
+
+var intMod 
+
+
+var chaMod 
+
+document.getElementById("str-mod") = function(){
+    (document.getElementById("str-mod").innerHTML = (document.getElementById("strScore")-10)/2;
+} 
