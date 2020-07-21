60fc9c1e9eedf5d8f10cd2a28b0ea512cdfa5cbb
Ryan Baker <ryan.shane.baker@gmail.com>
2020-07-21 18:05:59 -0400

    adding math functions for stats

--- character.js
+++ character.js
@@ -8,31 +8,21 @@ var charRace ='';
 var charClass='';
 
 //these score will drive the modifier that will play into the game mechanics
-var dexScore 
-var strScore 
-var conScore 
-var wisScore 
-var intScore 
-var chaScore 
+var dexScore = document.querySelector("#dexScore");
+var strScore = document.querySelector("#strScore");
+var conScore = document.querySelector("#conScore");
+var wisScore = document.querySelector("#wisScore");
+var intScore = document.querySelector("#intScore");
+var chaScore = document.querySelector("#chaScore");
 
 //equals corresponding Score value minus 10 then divided by 2 rounded down
-var dexMod = (dexScore-10)/2;
+dexScore.addEventListener("input", function(){
+    var dexMod = Math.floor((dexScore.value-10)/2);
+    var dexOutput = document.querySelector("#dexMod");
+    dexOutput.innerHTML=dexMod;})
 
+strScore.addEventListener("input", function(){
+    var strMod = Math.floor((strScore.value-10)/2);
+    var strOutput = document.querySelector("#strMod");
+    strOutput.innerHTML=strMod;})
 
-var strMod 
-
-
-var conMod 
-
-
-var wisMod 
-
-
-var intMod 
-
-
-var chaMod 
-
-document.getElementById("str-mod") = function(){
-    (document.getElementById("str-mod").innerHTML = (document.getElementById("strScore")-10)/2;
-} 
